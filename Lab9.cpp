74bacee733ba015f12f926a5442ac77f59cc48ca
EduardoA525 <eavila913@insite.4cd.edu>
2026-09-18 12:39:30 -0700

    Initial set up

---
+++ Lab9.cpp
@@ -0,0 +1,17 @@
+/*
+Write a program that will exercise STD::array functions. 
+Code a real-world simulation of something that could be simulated by an array. 
+The requirements are:
+
+- Use an STD::array
+- Have an external data file with at least 30 elements in it
+- Read the data into an STD::array 
+- Use a large variety of the <array> member functions from this module 
+  to demonstrate your understanding of the STD::array.
+- Then, as a second part of the same program (do not write another script please), 
+  essentially duplicate your existing code, adapting it to feature the STD::vector.
+*/
+
+//Eduardo Avila
+//COMSC - 210 - 5293
+//Lab 9 - STD::Array and STD::Vector
